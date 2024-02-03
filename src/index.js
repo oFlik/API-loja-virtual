e8615f38780cef7a1b1606e5f16aee011f7e3599
Thiago Pereira <iamthiago2@gmail.com>
2024-02-03 18:49:46 -0300

    Initial config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.listen(port);
+console.log(`API rodando na porta ${port}`);
