9b3fbe7f693f36593a8ea431720b9fbfb387ae40
Thiago Pereira <iamthiago2@gmail.com>
2024-02-03 21:42:47 -0300

    Unnecessary function call removed

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -1,5 +1,3 @@
-const { validateNewPassword } = require('../helpers/validateNewPassword');
-
 exports.validateRegisterBody = (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password) {
     return res.status(400).json({ message: 'Envie todos os campos obrigatórios!' });
