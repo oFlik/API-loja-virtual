42e8da1e1298c189aa7c98c4f8f16dd2544f2f0e
Thiago Pereira <iamthiago2@gmail.com>
2024-02-03 21:17:05 -0300

    Added user routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,9 @@
 const index = require('../routes/indexRoutes');
 const products = require('../routes/productRoutes');
+const users = require('../routes/userRoutes');
 
 module.exports = (app) => {
   app.use('/', index);
   app.use('/products', products);
+  app.use('/users', users);
 };
