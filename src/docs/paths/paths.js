c08ddd567d39f0f7792486c26288f6308c9436bf
Thiago Pereira <iamthiago2@gmail.com>
2024-02-09 13:39:56 -0300

    Added user and client paths

--- src/docs/paths/paths.js
+++ src/docs/paths/paths.js
@@ -1,9 +1,13 @@
 const index = require('./routes.js/index');
 const auth = require('./routes.js/auth');
+const user = require('./routes.js/user');
+const client = require('./routes.js/client');
 
 const paths = {
   index,
   auth,
+  user,
+  client,
 };
 
 module.exports = paths;
