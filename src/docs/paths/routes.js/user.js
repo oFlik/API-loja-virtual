4c395c43860068845ee587f70d8eb24689412e89
Thiago Pereira <iamthiago2@gmail.com>
2024-02-10 10:53:11 -0300

    Register completed

--- src/docs/paths/routes.js/user.js
+++ src/docs/paths/routes.js/user.js
@@ -1,10 +1,57 @@
 exports.register = {
-  post: {},
+  post: {
+    summary: 'Efetua o cadastro de um novo usuário',
+    description:
+      'O usuário deverá enviar nome, email e senha para cadastro, caso o email enviado já exista no banco de dados da aplicação, o cliente será convidado a enviar outro ou efetuar o login.',
+    operationId: 'userRegister',
+    security: {},
+    requestBody: {
+      description: 'Informações do usuário a ser cadastrado.',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/RegisterReqBody',
+          },
+        },
+      },
+      required: true,
+    },
+    responses: {
+      200: {
+        description: 'Mensagem de sucesso e dados do usuário cadastrado.',
+        content: {
+          'application/json': {
+            schema: {
+              properties: {
+                message: {
+                  enum: ['Cliente cadastrado com sucesso!'],
+                  type: 'string',
+                },
+                userData: {
+                  $ref: '#/components/schemas/UserData',
+                },
+              },
+              type: 'object',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/responses/duplicateEmail',
+      },
+      406: {
+        $ref: '#/components/responses/weakPassword',
+      },
+      500: {
+        $ref: '#/components/responses/error500',
+      },
+    },
+  },
 };
 
 exports.profile = {
   get: {
-    summary: 'Obter dados do usuário logado',
+    summary: 'Obtém dados do usuário logado',
     description:
       'O usuário deverá estar logado para acessar esta rota, os dados retornados sempre serão do usuário logado.',
     operationId: 'getProfile',
