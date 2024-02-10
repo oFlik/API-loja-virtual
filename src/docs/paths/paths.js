4cc3243fa8add8226d27a89db966c15ed20cb61e
Thiago Pereira <iamthiago2@gmail.com>
2024-02-10 12:30:11 -0300

    Added product paths

--- src/docs/paths/paths.js
+++ src/docs/paths/paths.js
@@ -2,11 +2,13 @@ const index = require('./routes.js/index');
 const auth = require('./routes.js/auth');
 const user = require('./routes.js/user');
 const client = require('./routes.js/client');
+const product = require('./routes.js/products')
 
 const paths = {
   index,
   auth,
   user,
+  product,
   client,
 };
 
