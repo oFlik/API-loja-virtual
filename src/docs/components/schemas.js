fdec947fe08bba83c97fa4b4528d95230bc65be6
Thiago Pereira <iamthiago2@gmail.com>
2024-02-10 10:08:03 -0300

    Refactoring

--- src/docs/components/schemas.js
+++ src/docs/components/schemas.js
@@ -1,7 +1,43 @@
-const auth = require('./routesRequestBody/auth');
-
-const schemas = {
-  auth,
+exports.loginReqBody = {
+  type: 'object',
+  properties: {
+    email: {
+      type: 'string',
+    },
+    password: {
+      type: 'string',
+    },
+  },
 };
 
-module.exports = schemas;
+exports.registerReqBody = {
+  type: 'object',
+  properties: {
+    name: {
+      type: 'string',
+    },
+    email: {
+      type: 'string',
+    },
+    password: {
+      type: 'string',
+    },
+  },
+};
+
+exports.userData = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'integer',
+    },
+    name: {
+      enum: ['Nome do usuário'],
+      type: 'string',
+    },
+    email: {
+      enum: ['Email do usuário'],
+      type: 'string',
+    },
+  },
+};
