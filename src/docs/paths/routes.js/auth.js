0ea5a56bbba53925dcf94c20b75b3a995a67b85a
Thiago Pereira <iamthiago2@gmail.com>
2024-02-10 10:25:14 -0300

    Better variable naming

--- src/docs/paths/routes.js/auth.js
+++ src/docs/paths/routes.js/auth.js
@@ -1,6 +1,6 @@
 exports.login = {
   post: {
-    summary: 'Efetuar o login de um usuário cadastrado',
+    summary: 'Efetua o login de um usuário cadastrado',
     description:
       'O usuário deverá enviar um email e senha válidos, que foram previamente cadastrados no banco de dados da aplicação, a rota irá então verificar as informações e retornar o token de usuário utilizado para autenticação nas demais rotas que o pedem.',
     operationId: 'userLogin',
@@ -10,7 +10,7 @@ exports.login = {
       content: {
         'application/json': {
           schema: {
-            $ref: '#/components/schemas/ReqBodyLogin',
+            $ref: '#/components/schemas/LoginReqBody',
           },
         },
       },
