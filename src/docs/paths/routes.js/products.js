7fea134078cee17adfc006cbde777143845d556c
Thiago Pereira <iamthiago2@gmail.com>
2024-02-10 12:30:44 -0300

    Finished list path

--- src/docs/paths/routes.js/products.js
+++ src/docs/paths/routes.js/products.js
@@ -1,11 +1,39 @@
-exports.list = {};
+exports.list = {
+  get: {
+    summary: 'Obtém as informações de todos os produtos cadastrados.',
+    description:
+      'Ao acessar esta rota, o usuário irá receber uma array contendo todas as informações sobre os produtos cadastrados no banco de dados.',
+    operationId: 'getProductList',
+    responses: {
+      200: {
+        description: 'Retorna uma array com os produtos cadastrados',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/components/schemas/ProductModel',
+              },
+            },
+          },
+        },
+      },
+      401: {
+        $ref: '#/components/responses/error401',
+      },
+      500: {
+        $ref: '#/components/responses/error500',
+      },
+    },
+  },
+};
 
-exports.add = {}
+exports.add = {};
 
-exports.edit = {}
+exports.edit = {};
 
-exports.detail = {}
+exports.detail = {};
 
-exports.delete = {}
+exports.delete = {};
 
-exports.types = {}
+exports.types = {};
